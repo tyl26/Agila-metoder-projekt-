13397159cce0d81624bafade68df901fa8de0724
Yvon Lascano <lascanoyvon@gmail.com>
2022-04-24 13:08:23 +0200

    #6

--- agila-metoder-projekt/src/components/Sprint.jsx
+++ agila-metoder-projekt/src/components/Sprint.jsx
@@ -2,10 +2,15 @@ function Sprint() {
     return (
         <section>
             <h1>what is sprint in scrum?</h1>
-            <p>Sprint is a short timebox when the team work to complete a certain set amount of work from backlog.
-                 In ohter words sprint is where you break down big and complex projects into bite-sized pieces.
-                 Which makes thing more manageable and alow the team work more faster while mainting the great quality.  </p>
+            <p>Sprint is a short timebox when the team work to complete a certain set amount of work from backlog. 
+                Sprint are fixed leght events of one month or less. 
+                In ohter words sprint is where you break down big and complex projects into bite-sized pieces.
+                Which makes thing more manageable and alow the team work more faster while mainting the great quality.
+                Sprint belong to one of the fove events in scrum, How ever sprint has a speciall case, where sprint is container 
+                where the remaining four events happens within the sprint.   </p>
                  <img src="https://www.toolsqa.com/gallery/Agile%20-%20Scrum/1.what%20is%20sprint%20-%20Sprint%20Cycle.png" alt="sprint Img" />
+                 
+                 <h6>See next page to read the remaining four events.</h6>
 
                  <section className="btns">
                 <Link to='/backlog'> <button>&#171;</button></Link>
