500a575132dee965c3a78e107f061be2b0fe0310
Yvon Lascano <lascanoyvon@gmail.com>
2022-04-24 14:27:01 +0200

    #2 alt. btns

--- agila-metoder-projekt/src/components/AlternativList.jsx
+++ agila-metoder-projekt/src/components/AlternativList.jsx
@@ -5,28 +5,29 @@ function AlternativList() {
         <section>
             <h1>What do you want to learn about?</h1>
 
-            <button>Start Guide from the beginning</button>
+
 
             <Link to='/scrum'>
+                <button>Start Guide from the beginning</button>
                 <button>What is Scrum?</button>
             </Link>
-            <Link to='/Allroles'>
+            <Link to='/roles'>
                 <button>All Roles</button>
             </Link>
-            <Link to='/Backlog'>
+            <Link to='/backlog'>
                 <button>Backlog</button>
             </Link>
             <Link to='/sprint'>
                 <button>Sprint</button>
             </Link>
-            <Link to='/Events'>
-                <button>All for events</button>
+            <Link to='/fourEvents'>
+                <button>All four events</button>
+            </Link>
+            <Link to='/artifacts'>
+                <button>Scrum Artefacts</button>
             </Link>
             <Link to='/agileest'>
                 <button>Agil Estamation</button>
-            </Link>
-            <Link to='/artefacts'>
-                <button>Scrum Artefacts</button>
             </Link>
         </section>
     );
