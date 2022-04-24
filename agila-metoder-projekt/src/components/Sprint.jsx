d618e8eab250961a80f4247efe1a635bb668d7dd
Yvon Lascano <lascanoyvon@gmail.com>
2022-04-24 12:54:56 +0200

    #6 sprint

---
+++ agila-metoder-projekt/src/components/Sprint.jsx
@@ -0,0 +1,13 @@
+function Sprint() {
+    return (
+        <section>
+            <h1>what is sprint in scrum?</h1>
+            <p>Sprint is a short timebox when the team work to complete a certain set amount of work from backlog.
+                 In ohter words sprint is where you break down big and complex projects into bite-sized pieces.
+                 Which makes thing more manageable and alow the team work more faster while mainting the great quality.  </p>
+                 <img src="https://www.toolsqa.com/gallery/Agile%20-%20Scrum/1.what%20is%20sprint%20-%20Sprint%20Cycle.png" alt="sprint Img" />
+        </section>
+    );
+}
+
+export default Sprint;
