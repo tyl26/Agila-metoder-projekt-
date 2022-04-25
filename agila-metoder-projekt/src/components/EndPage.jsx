0dbe302b792a16f8e240070945c9a5b310428a3b
Yvon Lascano <lascanoyvon@gmail.com>
2022-04-25 11:29:21 +0200

    commit

---
+++ agila-metoder-projekt/src/components/EndPage.jsx
@@ -0,0 +1,13 @@
+function EndPage() {
+    return ( 
+        <section>
+            <h1>Congrats!</h1>
+            <h5>You made it to the end!</h5>
+
+            <h6>If you want to challange/ test yourself, you can take a quiz her!</h6>
+            <a href="https://basicversity.com/quiz/agile-scrum-methodology"> Take scrum quiz!</a>
+        </section>
+     );
+}
+
+export default Endpage;
