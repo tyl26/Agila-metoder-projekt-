7bd691b18eedb67d98608a3b55d0b44c48ae1b41
Yvon Lascano <lascanoyvon@gmail.com>
2022-04-25 09:37:15 +0200

    #9 agile est.

---
+++ agila-metoder-projekt/src/components/AgileEst.jsx
@@ -0,0 +1,12 @@
+function AgileEst() {
+    return (
+        <section>
+            <h1>What is Agile Est</h1>
+            <p>
+                Agile estimation is the process for estimating the effort required to complete a prioritized task in the product backlog. This effort is usually measured with respect to the time it will take to complete that task, which, in turn, leads to accurate sprint planning.
+                Whether the team is developing a product or developing a project, we all need to answer “When will we be able to complete it?”, or to what extent we can do it at a certain point in time, so like the traditional development model, we need to estimate the amount of work before we start the project.</p>
+        </section>
+    );
+}
+
+export default AgileEst;
