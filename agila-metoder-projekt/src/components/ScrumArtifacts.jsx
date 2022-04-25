438c232dcf1ab9a1fde0ae7ad07ecdc53a5d2ff5
Yvon Lascano <lascanoyvon@gmail.com>
2022-04-25 11:52:32 +0200

    commit

--- agila-metoder-projekt/src/components/ScrumArtifacts.jsx
+++ agila-metoder-projekt/src/components/ScrumArtifacts.jsx
@@ -5,11 +5,11 @@ function ScrumArtirfacts() {
         <section>
             <Link to='/AlternativList'><button>Start</button></Link>
 
-            <h1>What is Scrum Artefacts?</h1>
+            <h1>What is Scrum Artifacts?</h1>
 
             <p>Scrum’s artifacts represent work or value. They are designed to maximize transparency of key information. Thus, everyone inspecting them has the same basis for adaptation.
 
-                Each artifact contains a commitment to ensure it provides information that enhances transparency and focus against which progress can be measured:
+                Each artifact contains a commitment to make sure that it provides information that it boosts its transparency and focuses against which progress can be measured:</p>
                 <ul>
                     <li> For the Product Backlog it is the Product Goal. This is where the " what to do"</li>
                     <li> For the Sprint Backlog it is the Sprint Goal. This is where the " what to take now + how"</li>
@@ -17,7 +17,7 @@ function ScrumArtirfacts() {
                     </li>
                 </ul>
 
-                These commitments exist to reinforce empiricism and the Scrum values for the Scrum Team and their stakeholders.</p>
+                <p> These engagement exist to build up empiricism and the Scrum values for the Scrum Team and their stakeholders.</p>
 
             <img className="artifactsImg" src="https://images.prismic.io/superpupertest/5c9293dd-c287-46b8-8aa5-32552a021247_Frame+2569.png?auto=compress,format" alt="scrum artifacts" />
 
